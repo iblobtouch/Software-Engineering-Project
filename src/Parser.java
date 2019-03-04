77b360f911a50f635a3e8772895f54980214e8c4
sohail butt <sohail786b@live.com>
2019-03-04 18:07:11 +0000

    Individual Variables now in array collection, refactored if statement to for loop

--- src/Parser.java
+++ src/Parser.java
@@ -22,7 +22,7 @@ public class Parser
 {
     private CommandWords commands;  // holds all valid command words
     private Scanner reader;         // source of command input
-
+    
     /**
      * Create a parser to read from the terminal window.
      */
@@ -40,10 +40,10 @@ public class Parser
      */
     public Command getCommand() 
     {
+        
         String inputLine;   // will hold the full input line
-        String word1 = null;
-        String word2 = null;
-        String word3 = null;
+        String[] wordList = {null, null, null};
+
 
         System.out.print("> ");     // print prompt
 
@@ -51,24 +51,18 @@ public class Parser
 
         // Find up to two words on the line.
         Scanner tokenizer = new Scanner(inputLine);
-        if(tokenizer.hasNext()) {
-            word1 = tokenizer.next();      // get first word
-            if(tokenizer.hasNext()) {
-                word2 = tokenizer.next();      // get second word
-            }
-            if(tokenizer.hasNext()) {
-                word3 = tokenizer.next();      // get second word
-                // note: we just ignore the rest of the input line.
-            }
+        
+        for(int i=0; tokenizer.hasNext(); i++){
+            wordList[i] = tokenizer.next();
         }
 
         // Now check whether this word is known. If so, create a command
         // with it. If not, create a "null" command (for unknown command).
-        if(commands.isCommand(word1)) {
-            return new Command(word1, word2, word3);
+        if(commands.isCommand(wordList[0])) {
+            return new Command(wordList[0], wordList[1], wordList[2]);
         }
         else {
-            return new Command(null, word2, word3); 
+            return new Command(null, wordList[1], wordList[2]); 
         }
     }
 }
